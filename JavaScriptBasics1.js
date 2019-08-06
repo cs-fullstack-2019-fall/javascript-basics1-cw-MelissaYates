521eb37785ceb02443acace2b49177ce8b409974
Melissa Yates <melissampeart@gmail.com>
2019-08-06 16:05:59 -0500

    first graded assignment

---
+++ JavaScriptBasics1.js
@@ -0,0 +1,7 @@
+alert("Hello World");
+var nameVariable = "Melissa";
+alert("Hello " + nameVariable);
+var dogAge = 5;
+alert("Your dog is: " + dogAge * 7)
+var number = 2;
+alert("1 + 1 = " + number);
